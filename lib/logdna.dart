b4e3c51d3de19f4949e6c0921bbafd95b2937a0d
MXilian <imxv@ya.ru>
2023-01-16 14:27:49 +0300

    Tag support added

--- lib/logdna.dart
+++ lib/logdna.dart
@@ -17,12 +17,15 @@ class LogDNA {
   LogDNA({required this.apiKey, required this.hostName});
 
   //// Sends the log via the logdna ingest API
-  Future<DnaResponse> log(DnaLine line) async {
-    final now = DateTime.now().toUtc().millisecondsSinceEpoch;
+  Future<DnaResponse> log(DnaLine line, {List<String>? tags}) async {
+    final now = DateTime.now().toUtc().millisecondsSinceEpoch / 100;
     try {
-      final uri =
-          Uri.parse("https://logs.logdna.com/logs/ingest?hostname=$hostName"
-              "&now=$now&apikey=$apiKey");
+      String uriString = "https://logs.mezmo.com/logs/ingest?hostname=$hostName"
+          "&now=$now&apikey=$apiKey";
+      if (tags != null) {
+        uriString += '&tags=${tags.join(",")}';
+      }
+      final uri = Uri.parse(uriString);
 
       http.Response response = await http.post(uri,
           body: jsonEncode({
@@ -35,7 +38,8 @@ class LogDNA {
 
       if (response.statusCode == 200) {
         print(true);
-        return DnaResponse(true, response.body);
+        var result = DnaResponse(true, response.body);
+        return result;
       } else {
         print(true);
         return DnaResponse(false, response.body);
--- test/logdna_test.dart
+++ test/logdna_test.dart
@@ -1,7 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:logdna/logdna.dart';
+import 'package:logdna/models/dna_line.dart';
 
 void main() {
-
+  test("test logDna", () async {
+    final _logDna = LogDNA(
+      apiKey: 'a92ad6d21931a030d60a224289b14f17',
+      hostName: 'android',
+    );
+    await _logDna.log(DnaLine(
+      timestamp: (DateTime.now().millisecondsSinceEpoch / 100).toString(),
+      line: 'line',
+      app: 'testApp',
+      level: DnaLevel.info,
+      env: 'dev',
+      meta: {'meta': 'body'},
+    ), tags: ['test1, test2']);
+  });
 }
